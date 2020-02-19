c64eeb40160bd3337d15ff7c137c9497f9c6d2a2
ryosuke0513 <daendo0513@gmail.com>
2020-02-19 16:54:57 +0900

    Update database.php
    dbupdate

--- konasaHP/database.php
+++ konasaHP/database.php
@@ -6,17 +6,21 @@ require '../vendor/autoload.php';
 
 //DB接続処理
 function dbConnect(){
-  $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
-  $db['dbname'] = ltrim($db['path'], '/');
-  $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
-  $user = $db['user'];
-  $password = $db['pass'];
-  $options = array(
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
-  );
-  $dbh = new PDO($dsn,$user,$password,$options);
+  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"], 1);
+
+  $dbh = new PDO(
+    'mysql:host=' . $server . ';dbname=' . $db . ';charset=utf8mb4',
+    $username,
+    $password,
+    [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+    ]);
   return $dbh;
 }
 
