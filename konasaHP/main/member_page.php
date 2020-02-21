34bd177198b2a29bb8df34b50e25ee3123915c0e
ryosuke0513 <daendo0513@gmail.com>
2020-02-21 09:18:55 +0900

    re
    re

--- konasaHP/main/member_page.php
+++ konasaHP/main/member_page.php
@@ -36,34 +36,39 @@ while($row=$stt->fetch()){
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>マイページ</title>
+  <title>メンバーリスト</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet/mypage_sp.css">
+  <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 </head>
 <body>
-  <div>
-    <p>My page</p>
-    <div>
+  <div class="top">
+    <div class="proimg">
+      <img src="data:image/jpeg;base64,<?php echo $result['proimg'];?>">
+    </div>
+    <p class="top_p" style="padding-bottom:20px;"></p>
+    <div class="mymain">
       <p>Name</p>
-      <p><?php if(!empty($result)){echo $result['name'];}else{echo "登録ページで名前を".$errorMessage;}?></p>
+      <p><?php if(!empty($result)){echo $result['name'];}else{echo "登録されていません".$errorMessage;}?></p>
     </div>
-    <div>
+    <div class="mymain">
       <p>Work</p>
-      <p><?php if(!empty($result)){echo $result['work'];}else{echo "登録ページでお仕事を".$errorMessage;}?></p>
+      <p><?php if(!empty($result)){echo $result['work'];}else{echo "登録されていません".$errorMessage;}?></p>
     </div>
-    <div>
+    <div class="mymain">
       <p>Skill</p>
-      <p><?php if(!empty($result)){echo $result['skill'];}else{echo "登録ページでスキルを".$errorMessage;}?></p>
+      <p><?php if(!empty($result)){echo $result['skill'];}else{echo "登録されていません".$errorMessage;}?></p>
     </div>
-    <div>
+    <div class="mymain">
       <p>Assist</p>
-      <p><?php if(!empty($result)){echo $result['assist'];}else{echo "登録ページで支援できることを".$errorMessage;}?></p>
+      <p><?php if(!empty($result)){echo $result['assist'];}else{echo "登録登録されていません".$errorMessage;}?></p>
     </div>
-    <div>
+    <div class="mymain">
       <p>Support</p>
-      <p><?php if(!empty($result)){echo $result['support'];}else{echo "登録ページで支援してほしいことを".$errorMessage;}?></p>
+      <p><?php if(!empty($result)){echo $result['support'];}else{echo "登録されていません".$errorMessage;}?></p>
     </div>
     <div>
       <form action="" method="post">
-        <input type="button" onclick="location.href='all_member.php'" value="メンバーリスト">
+        <input class="fas" type="button" onclick="history.back()" value="&#xf2ea;" style="background-image: linear-gradient(45deg, #FF9999 0%, #91fdb7 100%);">
       </form>
   </div>
 </body>
